d4e450eb820276902cc8c4e4e3128e95f0e4602a
Charlon Palacay <charlon@gmail.com>
2019-12-19 14:36:05 -0800

    Delete request.py

--- mongo_rest/request.py
+++
@@ -1,5 +0,0 @@
-spot = Spot.objects.create(
-    name="My Cool Spot",
-    location={ type: "Point", coordinates: [-73.856077, 40.848447] },
-    hours=[SpotAvailableHours(day="monday", start_time="10am", end_time="11pm"])
-)
