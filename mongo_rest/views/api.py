ac4eac708720e3b4efbe22421e4332381349dee9
Craig M. Stimmel <cstimmel@uw.edu>
2020-01-10 15:41:11 -0800

    lookup with pipeline

--- mongo_rest/views/api.py
+++ mongo_rest/views/api.py
@@ -6,6 +6,7 @@ from dotenv import load_dotenv
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 from bson.json_util import dumps
+import json
 load_dotenv()
 
 
@@ -25,9 +26,22 @@ class SpotAPIView(View):
         spots = db.spots
 
         # example queries - ids are excluded from the final results
-        query = spots.find({})
+        #query = spots.find({})
         # query = spots.find({"_id": ObjectId("5e0e2d2ec852de2cd7e0cfe4")}, {"_id": 0})
         # query = spots.find({"building.code": "KNE"}, {"_id": 0})
+        pipeline = [{ '$match': { 'type': 'food truck' } 
+                    },
+                    {
+                      '$lookup':
+                        {
+                            'from': 'buildings',
+                            'localField': 'building_id',
+                            'foreignField': '_id',
+                            'as': 'blah'
+                            }
+                        }
+                   ]
+        query = spots.aggregate(pipeline)
 
         # bson.json_util "dumps" - used for converting bson data to json
         results = query
