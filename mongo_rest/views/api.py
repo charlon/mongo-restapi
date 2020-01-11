76243fdb2d2ac4d4561aa19c19f0679756562b35
Charlon Palacay <charlon@gmail.com>
2020-01-10 22:25:07 -0800

    Use cleaner code for aggregate pipeline.

--- mongo_rest/views/api.py
+++ mongo_rest/views/api.py
@@ -13,7 +13,7 @@ load_dotenv()
 class SpotAPIView(View):
     def get(self, request):
 
-        # connect to 'mongo_rest_db' via internal network
+        # connect to "mongo_rest_db" via internal network
         client = MongoClient("mongodb://mongo_rest_db:27017/")
 
         # authenticate to mongodb
@@ -26,22 +26,22 @@ class SpotAPIView(View):
         spots = db.spots
 
         # example queries - ids are excluded from the final results
-        #query = spots.find({})
+        # query = spots.find({})
         # query = spots.find({"_id": ObjectId("5e0e2d2ec852de2cd7e0cfe4")}, {"_id": 0})
         # query = spots.find({"building.code": "KNE"}, {"_id": 0})
-        pipeline = [{ '$match': { 'type': 'food truck' } 
-                    },
-                    {
-                      '$lookup':
-                        {
-                            'from': 'buildings',
-                            'localField': 'building_id',
-                            'foreignField': '_id',
-                            'as': 'blah'
-                            }
-                        }
-                   ]
-        query = spots.aggregate(pipeline)
+
+   
+        query = spots.aggregate([
+            {"$match": {}},
+            {"$lookup":
+                {
+                    "from": "buildings",
+                    "localField": "building_id",
+                    "foreignField": "_id",
+                    "as": "blah"
+                }
+            },
+        ])
 
         # bson.json_util "dumps" - used for converting bson data to json
         results = query
